81ea6303ba22ccf2bc6b37b5a073186305b818b2
AK46rocks <ak46rocks@gmail.com>
2023-06-16 01:27:07 +0530

    testest

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare class DummyAPI {
+    static CELO_API_ENDPOINT: string;
+    static MOONBEAM_API_ENDPOINT: string;
+    static MOONRIVER_API_ENDPOINT: string;
+    static CeloChartAPI(): Promise<any>;
+    static MoonbeamChartAPI(): Promise<any>;
+    static MoonriverChartAPI(): Promise<any>;
+}
