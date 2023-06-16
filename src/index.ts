450d6eb819755d6e2602fac49de536ad64fa3373
AK46rocks <ak46rocks@gmail.com>
2023-06-16 19:49:25 +0530

    typescript error fixed

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
 export class DummyAPI {
-  static CELO_API_ENDPOINT =
-    "https://api.dapplooker.com/chart/dc9b69d8-7ca1-45d4-8ad0-a17f915f3f03";
-  static MOONBEAM_API_ENDPOINT =
-    "https://api.dapplooker.com/chart/8c6d43e4-1677-4934-bac7-a87e13c2f98b";
-  static MOONRIVER_API_ENDPOINT =
-    "https://api.dapplooker.com/chart/1630a0d6-f025-40cb-912b-bd9fd1f09ff2";
-
-  static async CeloChartAPI() {
-    let responseObj = await fetch(this.CELO_API_ENDPOINT);
-    let data = await responseObj.json();
+  static async getCeloChartAPI(): Promise<CeloInterface> {
+    let responseObj = await fetch(
+      "https://api.dapplooker.com/chart/dc9b69d8-7ca1-45d4-8ad0-a17f915f3f03"
+    );
+    let data: CeloInterface = await responseObj.json();
     return data;
   }
 
-  static async MoonbeamChartAPI() {
-    let responseObj = await fetch(this.MOONBEAM_API_ENDPOINT);
-    let data = await responseObj.json();
+  static async getDummyData(): Promise<DummyData> {
+    let responseObj = await fetch("https://dummyjson.com/products/1");
+    let data: DummyData = await responseObj.json();
     return data;
   }
+}
 
-  static async MoonriverChartAPI() {
-    let responseObj = await fetch(this.MOONRIVER_API_ENDPOINT);
-    let data = await responseObj.json();
-    return data;
-  }
+export interface CeloInterface {
+  data: {
+    rows: [];
+    cols: [];
+    insights: [];
+    results_timezone: string;
+  };
+  json_query: Object;
+  status: string;
+}
+export interface DummyData {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: Array<String>;
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
 }
